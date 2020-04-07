20176ac072ea12e48d91875eaa8b55a257f3b4a1
shanquane <2495742347@qq.com>
2020-04-07 10:49:19 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # export-wechat-voices
+
+## Description
+This repo is mostly based on [silk-v3-decoder](https://github.com/kn007/silk-v3-decoder), it supports exporting your wechat silk voice files to timestamp named mp3 files.
+
+This repo only works on Linux/Unix, not for windows(you can use wsl instead).
+
+My family used to send lots of voice messages on wechat, Then I've found it's hard to save and play them outside of wechat. That's why I write this script.
+
+## Requirement
+
+* gcc
+* ffmpeg
+
+## How To Use
+1. copy voice Directory from your phone to PC.
+    The directory locations are as below for now:
+    - For Android:/storage/emulated/0/tencent/MicroMsg/c5bxxx/voice2/../xxx.amr
+2. `git clone https://github.com/shanquan/export-wechat-voices & cd export-wechat-voices`
+3. `sh convert.sh pathToVoiceDir`
+
+After the upper command, you will see your mp3 voice files in the `output` directory. 
+
+## 中文说明
+本项目基于[silk-v3-decoder](https://github.com/kn007/silk-v3-decoder)开发，以支持将微信里的所有语音文件按时间顺序导出mp3格式。
+
+我们家人经常在微信中发送语音消息，我很希望能够保存这些语音（关于家人的数字记忆），但是却发现导出并播放这些语音文件有点麻烦，于是写了这个脚本。
+
+## 依赖组件
+
+* gcc
+* ffmpeg
+
+## 如何使用
+1. 复制手机里的微信语音文件存放路径至电脑。
+   - Android手机目录voice2：/storage/emulated/0/tencent/MicroMsg/c5bxxx/voice2/../xxx.amr
+2. `git clone https://github.com/shanquan/export-wechat-voices & cd export-wechat-voices`
+3. `sh convert.sh pathToVoiceDir`
+
+脚本命令执行完毕后，可以在`output`文件夹下查看mp3格式的语音文件。
---
+++ convert.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# File: convert.sh
+# Date: April 3rd, 2020
+# Time: 16:07 +0800
+# Author: sq <2495742347@qq.com>
+# Usage: 
+# 1. copy /storage/emulated/0/tencent/MicroMsg/c5bxxx/voice2/ to your PC.
+# 2. git clone https://github.com/shanquan/export-wechat-voices
+# 4. sh convert.sh pathTovoice2
+# Requirement: gcc ffmpeg
+
+# Colors
+RED="$(tput setaf 1 2>/dev/null || echo '\e[0;31m')"
+GREEN="$(tput setaf 2 2>/dev/null || echo '\e[0;32m')"
+YELLOW="$(tput setaf 3 2>/dev/null || echo '\e[0;33m')"
+WHITE="$(tput setaf 7 2>/dev/null || echo '\e[0;37m')"
+RESET="$(tput sgr 0 2>/dev/null || echo '\e[0m')"
+
+function convert(){
+[ ! -d "$1" ]&&echo -e "${RED}[Error]${RESET} Input folder not found, please check it."&&exit
+for file in `find $1 -name "*.amr"`
+do
+ if [ -d $1"/"$file ]
+ then
+ read_dir $1"/"$file
+ else
+ # full path
+ #echo `pwd`"/"$file
+ # last modify timestamp
+ LAST_MODIFY_TIMESTAMP=`stat -c %Y  $file`
+ formart_date=`date '+%Y-%m-%d-%H%M%S' -d @$LAST_MODIFY_TIMESTAMP`
+ # append to log file
+ # see ref: https://www.cnblogs.com/hurryup/articles/10241601.html
+ FILE_NAME=${file##*/}
+ if [ ${FILE_NAME:0:5} != "msg__" ];then
+   #echo "$formart_date,$FILE_NAME" >> output/data.log
+   #cp $file "output/$formart_date.amr"
+   transform $file "$formart_date"
+ fi
+ fi
+done
+}
+
+function clear_output(){
+  if [ -d "output" ];then
+  rm -rf output
+  fi
+  mkdir output
+}
+
+function transform(){
+  [[ ! -z "$(pidof ffmpeg)" ]]&&echo -e "${RED}[Error]${RESET} ffmpeg is occupied by another application, please check it."&&exit
+  let CURRENT+=1
+		$cur_dir/silk/decoder $1 "output/$2.pcm" > /dev/null 2>&1
+		if [ ! -f "output/$2.pcm" ]; then
+			ffmpeg -y -i "$1" "output/$2.mp3" > /dev/null 2>&1 &
+			ffmpeg_pid=$!
+			while kill -0 "$ffmpeg_pid"; do sleep 1; done > /dev/null 2>&1
+			[ -f "output/$2.mp3" ]&&echo -e "[$CURRENT]${GREEN}[OK]${RESET} Convert $1 to output/$2.mp3 success, ${YELLOW}but not a silk v3 encoded file.${RESET}"&&continue
+			echo -e "[$CURRENT]${YELLOW}[Warning]${RESET} Convert $1 false, maybe not a silk v3 encoded file."&&continue
+		fi
+		ffmpeg -y -f s16le -ar 24000 -ac 1 -i "output/$2.pcm" "output/$2.mp3" > /dev/null 2>&1 &
+		ffmpeg_pid=$!
+		while kill -0 "$ffmpeg_pid"; do sleep 1; done > /dev/null 2>&1
+		rm "output/$2.pcm"
+		[ ! -f "output/$2.mp3" ]&&echo -e "[$CURRENT]${YELLOW}[Warning]${RESET} Convert $1 false, maybe ffmpeg no format handler for $3."&&continue
+		echo -e "[$CURRENT]${GREEN}[OK]${RESET} Convert $1 To output/$2.mp3 Finish."
+}
+
+# test on wsl ubuntu 18.04
+# Main
+cur_dir=$(cd `dirname $0`; pwd)
+CURRENT=0
+
+if [ ! -r "$cur_dir/silk/decoder" ]; then
+	echo -e "${WHITE}[Notice]${RESET} Silk v3 Decoder is not found, compile it."
+	cd $cur_dir/silk
+	make && make decoder
+	[ ! -r "$cur_dir/silk/decoder" ]&&echo -e "${RED}[Error]${RESET} Silk v3 Decoder Compile False, Please Check Your System For GCC."&&exit
+	echo -e "${WHITE}========= Silk v3 Decoder Compile Finish =========${RESET}"
+fi
+
+cd $cur_dir
+clear_output
+convert $1
